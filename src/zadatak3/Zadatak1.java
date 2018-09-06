f69a1798708af41a610c254c1db02a18157732d7
Radovan Olujic <Radovan.Olujic@endava.com>
2018-09-06 14:25:19 +0200

    dodajem treci zadatak

---
+++ src/zadatak3/Zadatak1.java
@@ -0,0 +1,55 @@
+package zadatak3;
+import java.util.Scanner;
+
+public class Zadatak1 {
+	static int a;
+	static int b;
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("Unesite prvi broj: ");
+		a = unosInt();
+		System.out.println("Unesite drugi broj: ");
+		b = unosInt();
+		sc.close();
+		
+		if (b < a) {
+			System.out.println("Parni brojevi su: ");
+			while (b <= a) {
+
+				if (b % 2 == 0) {
+					System.out.println(b);
+				}
+				b++;
+			}
+
+		}
+
+		else if (a < b) {
+			System.out.println("Parni brojevi su: ");
+			while (a <= b) {
+
+				if (a % 2 == 0) {
+					System.out.println(a);
+				}
+				a++;
+			}
+		} else {
+			if (a % 2 == 0) {
+				System.out.println("Broj " + a + " je paran.");
+			} else {
+				System.out.println("Nema parnih brojeva!");
+			}
+		}
+	}
+
+	private static int unosInt() {
+		while (!sc.hasNextInt()) {
+			System.out.println("Pogresan unos! Pokusajte ponovo.");
+			sc.next();
+		}
+		return sc.nextInt();
+	}
+}
